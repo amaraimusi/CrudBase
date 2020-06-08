bfe058de13f3db529944a8a897985bd62d0dc40c
user <amaraimusi@gmail.com>
2020-06-08 09:06:19 +0900

    for cake

---
+++ wp/mng_index.php
@@ -0,0 +1,16 @@
+<?php 
+
+// global $crudBaseWp;
+
+// $scr_key = 'Client';
+// if(!empty($_GET['scr_key'])){
+// 	$scr_key = $_GET['scr_key'];
+// }
+// $crudBaseWp->showPage($scr_key);
+
+?>
+<h2>アクティビティ見積もりシステム</h2>
+
+
+<a href="/actiestim/mng/">管理サイト</a>
+
