bcb341d3aac0763d8575e777504f41916ca1ce7a
user <amaraimusi@gmail.com>
2020-06-08 09:34:04 +0900

    最初のwp_demo、 最初のディストリビューション(dist)、 cake_to_common.sh

---
+++ shell/02_cake_to_dist.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/sh
+echo 'cake_demoのCrudBase関連ファイルをディストリビューションフォルダ(distフォルダ）に上書きコピーします。'
+echo '更新日付が新しいファイルだけ上書きします。'
+
+rsync -auvz ../cake_demo/app/Vendor/CrudBase ../dist/CrudBase/php
+rsync -auvz ../cake_demo/app/webroot/js/CrudBase ../dist/CrudBase/js
+rsync -auvz ../cake_demo/app/webroot/css/CrudBase ../dist/CrudBase/css
+
+
+echo "------------ 上書きコピー完了"
+cmd /k
