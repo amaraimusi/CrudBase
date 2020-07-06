78433e8b3739dee461f8911bb3fec6d37e2b782e
user <amaraimusi@gmail.com>
2020-07-06 16:15:10 +0900

    ログイン画面

--- laravel7/dev/routes/web.php
+++ laravel7/dev/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 	Route::get('neko/bark', 'NekoController@bark');
 	Route::get('neko', 'NekoController@index');
 	Route::get('neko2', 'NekoController@index2');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
