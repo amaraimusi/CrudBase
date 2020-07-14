e8f2a96e5d12d6cfcb9fddf0c2100fab0190efba
user <amaraimusi@gmail.com>
2020-07-14 11:40:38 +0900

    GulpでCSSファイル群をCrudBase.min.css一つにまとめる。

---
+++ dist/CrudBase/gulp/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var terser = require('gulp-terser');
+
+gulp.task('CrudBase', function () {
+	gulp.src('../js/CrudBase/*.js')
+	.pipe(concat('CrudBase.min.js'))
+	.pipe(terser())
+	.pipe(gulp.dest('../js/CrudBase/dist'));
+});
+
+gulp.task('CrudBaseForCss', function () {
+	gulp.src('../css/CrudBase/*.css')
+	.pipe(concat('CrudBase.min.css'))
+	.pipe(gulp.dest('../css/CrudBase/dist'));
+});
