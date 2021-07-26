0340e9451b882a2ad8ec1fe9852911d4c54386ba
user <user@host.docker.internal>
2021-07-26 21:44:33 +0900

    homeをダッシュボードにする

--- laravel7/dev/routes/web.php
+++ laravel7/dev/routes/web.php
@@ -12,10 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//■■■□□□■■■□□□
+// Route::get('/', function () {
+// 	return view('welcome');
+// });
 
-Route::get('/', function () {
-	return view('welcome');
-});
+	Route::get('/', 'DashboardController@index');
 	
 	
 	Route::post('ajax_login_with_cake/login_check', 'AjaxLoginWithCakeController@login_check');
