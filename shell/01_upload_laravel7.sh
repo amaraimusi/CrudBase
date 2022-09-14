bdaa688934bd96bc2425c606c1cabdd261916c33
amaraimusi <amaraimusi@gmail.com>
2022-09-14 11:28:38 +0900

    パッケージ化のため旧モジュールを一旦すべて除去。Composerで利用できるパッケージにするため。

--- shell/01_upload_laravel7.sh
+++
@@ -1,9 +0,0 @@
-﻿#!/bin/sh
-echo 'ソースコードを差分アップロードします。'
-
-rsync -auvz ../dist amaraimusi@amaraimusi.sakura.ne.jp:www/CrudBase
-rsync -auvz --exclude='.env' ../laravel7 amaraimusi@amaraimusi.sakura.ne.jp:www/CrudBase
-
-
-echo "------------ 送信完了"
-#cmd /k
